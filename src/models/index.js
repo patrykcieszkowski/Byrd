14156cf5d563527d6b9eccb28c9c8522cf793725
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-04-16 18:35:55 +0100

    fixed import syntax for ChatEvents

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ import User from './User'
 import Company from './Company'
 import Widget from './Widget'
 import Chat from './Chat'
-import * as ChatEvent from './Chat_event'
+import ChatEvent from './Chat_event'
 
 export default {
   User,
